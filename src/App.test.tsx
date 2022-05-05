ee0a7ddd3b7ba4c007ed13bbd0cfb13dbc8a0456
Guga Zimmermann <gugazimmermann@gmail.com>
2022-05-05 16:06:06 -0300

    sample page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders email', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/contato@pocz.io/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <img src={process.env.PUBLIC_URL + '/logo512-circle.png'} alt="logo" />
       </header>
+      <main className="app-email">
+        <a href='mailto:contato@pocz.io'>[ contato@pocz.io ]</a>
+      </main>
     </div>
   );
 }
